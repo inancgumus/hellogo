cdbd64a6ff7e4c749a5414bd4aa83c5043625b29
Inanc Gumus <m@inanc.io>
2018-11-21 13:00:24 +0300

    add: main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+// makes this an executable Go program
+package main
+
+// imports the "fmt" package, so you can print
+import "fmt"
+
+// go first executes this function. it's called: "main function" or "func main"
+func main() {
+	// this calls Println function from the fmt package
+	// with one argument: "Hello World!"
+	fmt.Println("Hello World!")
+
+	// EXERCISE:
+	// Let's print your name and your age by separating them with commas.
+
+	// TYPE YOUR CODE BELOW
+
+	// Here's the solution:
+	// https://play.golang.org/p/DpG9s92iyhh
+}
